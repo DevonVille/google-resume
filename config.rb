0e24459e47c2ea027d5ce1b1315b29975f4c0358
Elad Rom <webdev@eladrom.org>
2014-03-30 03:03:34 +0300

    initial commit

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+
+preferred_syntax = :scss
+sprite_engine = :oily_png
+project_type = :stand_alone
+environment = :development
+output_style = :nested
+line_comments = false
